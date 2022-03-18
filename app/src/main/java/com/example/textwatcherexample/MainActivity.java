e7142ee4f30d114fd347a447e4e007726ff68f4a
jonathasmedina <emaildojonathas@gmail.com>
2022-03-18 11:45:28 -0400

    Initial commit

---
+++ app/src/main/java/com/example/textwatcherexample/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.textwatcherexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText ed1;
+    TextView tv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ed1 = findViewById(R.id.editText1);
+        tv1 = findViewById(R.id.textView);
+
+        ed1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String palavra = ed1.getText().toString().trim();
+                tv1.setText(palavra);
+            }
+        });
+    }
+}
